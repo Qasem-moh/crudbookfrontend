9c5564214f5a426a71a8c8ebf9742a504fc3600d
Qasem Mohammd <75634309+Qasem-moh@users.noreply.github.com>
2024-02-06 20:04:59 +0300

    crud

---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,44 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+function NavBar(props) {
+    return (<>
+            {/*<Navbar bg="dark" data-bs-theme="dark">*/}
+            {/*    <Container>*/}
+            {/*        <Navbar.Brand href="#home">Navbar</Navbar.Brand>*/}
+            {/*        <Nav className="me-auto">*/}
+            {/*            <Nav.Link href="#home">Home</Nav.Link>*/}
+            {/*            <Nav.Link href="#features">Features</Nav.Link>*/}
+            {/*            <Nav.Link href="#pricing">Pricing</Nav.Link>*/}
+            {/*        </Nav>*/}
+            {/*    </Container>*/}
+            {/*</Navbar>*/}
+
+            {/*<br/>*/}
+            {/*<Navbar bg="primary" data-bs-theme="dark">*/}
+            {/*    <Container>*/}
+            {/*        <Navbar.Brand href="#home">Navbar</Navbar.Brand>*/}
+            {/*        <Nav className="me-auto">*/}
+            {/*            <Nav.Link href="#home">Home</Nav.Link>*/}
+            {/*            <Nav.Link href="#features">Features</Nav.Link>*/}
+            {/*            <Nav.Link href="#pricing">Pricing</Nav.Link>*/}
+            {/*        </Nav>*/}
+            {/*    </Container>*/}
+            {/*</Navbar>*/}
+
+            <br/>
+            <Navbar bg="light" data-bs-theme="light">
+                <Container>
+                    <Navbar.Brand href="/">Store <strong>Books</strong></Navbar.Brand>
+                    <Nav className="me-auto">
+                        <Nav.Link href="/">Home</Nav.Link>
+                        <Nav.Link href="/add">Add_<strong>Books</strong></Nav.Link>
+                        <Nav.Link href="#pricing"><strong>Delete</strong></Nav.Link>
+                    </Nav>
+                </Container>
+            </Navbar>
+        </>);
+}
+
+export default NavBar;
