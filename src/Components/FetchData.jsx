47e19b5888555276395c4db7af45f17e4733d39e
Qasem Mohammd <75634309+Qasem-moh@users.noreply.github.com>
2024-02-07 00:53:46 +0300

    crud

---
+++ src/Components/FetchData.jsx
@@ -0,0 +1,42 @@
+import React, {useEffect, useReducer} from 'react';
+import axios from "axios";
+
+const initialState = {
+    loading: true, error: '', books: {}
+}
+const booksReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case 'FETCH_BOOKS':
+            return {
+                loading: false, error: '', books: action.payload
+            }
+        case 'ERROR_FETCH_BOOK':
+            return {
+                loading: false, error: '', books: {}
+            }
+        default:
+            return state;
+    }
+}
+
+function FetchData(props) {
+    const [state, dispatch] = useReducer(booksReducer, initialState);
+    useEffect(() => {
+        axios.get('https://crud-yqyn.onrender.com/getallcrudbooks')
+            .then(response => {
+                dispatch({
+                    type: 'FETCH_BOOKS', payload: response.data
+                })
+            }).catch(err => {
+            dispatch({
+                type: 'ERROR_FETCH_BOOK', payload: err
+            })
+        })
+    },[])
+    console.log(state.books)
+    return (<div>
+        {state.loading?<p>Loading...</p>:<p>Done</p>}
+    </div>);
+}
+
+export default FetchData;
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import FetchData from "./FetchData";
 
 function Home(props) {
     return (
         <div>
             <h5>Home</h5>
+            <FetchData/>
         </div>
     );
 }
